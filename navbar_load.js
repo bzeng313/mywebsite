0fcf8c1fdf652734933dd9e0e2be1cfc71c508ef
Brian Zeng <brianzeng@DN0a1fe189.SUNet>
2019-02-23 02:31:36 -0800

    new

--- navbar_load.js
+++ navbar_load.js
@@ -2,22 +2,22 @@ $(document).ready(
     function() {
         $("nav").load("navbar.html");
     }
-        // function() {
-    //     switch($("title").text()) {
-    //         case "Home":
-    //             jQuery('#home').addClass('active');
-    //             break;
-    //         case "Projects":
-    //             jQuery('#projects').addClass('active');
-    //             break;
-    //         case "Contact":
-    //             jQuery('#contacts').addClass('active'); 
-    //             break;
-    //         case "About":
-    //             jQuery('#about').addClass('active');
-    //             break;
-    //     }
-    // }
+    function() {
+        switch($("title").text()) {
+            case "Home":
+                jQuery('#home').addClass('active');
+                break;
+            case "Projects":
+                jQuery('#projects').addClass('active');
+                break;
+            case "Contact":
+                jQuery('#contacts').addClass('active'); 
+                break;
+            case "About":
+                jQuery('#about').addClass('active');
+                break;
+        }
+    }
 );
 
 
