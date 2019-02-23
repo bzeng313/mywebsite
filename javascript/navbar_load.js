bd8f9ed430ff24735f78075b2905bc2d54216744
Brian Zeng <brianzeng@DN0a1fe189.SUNet>
2019-02-23 03:39:49 -0800

    functioning website for now

---
+++ javascript/navbar_load.js
@@ -0,0 +1,25 @@
+//TODO: find better way of detecting particular webpage
+$(document).ready(
+    function() {
+        $("nav").load("../navbar.html", function() {
+                switch($("title").text()) {
+                    case "Home":
+                        jQuery('#home').addClass('active');
+                        break;
+                    case "Projects":
+                        jQuery('#projects').addClass('active');
+                        break;
+                    case "Contact":
+                        jQuery('#contact').addClass('active'); 
+                        break;
+                    case "About":
+                        jQuery('#about').addClass('active');
+                        break;
+                }
+        });
+    }
+);
+
+
+
+
