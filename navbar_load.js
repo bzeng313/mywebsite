343e32b9d748e109c6c3c8eed81d4c9b91b7f89b
Brian Zeng <brianzeng@DN0a1fe189.SUNet>
2019-02-23 02:10:24 -0800

    new

--- navbar_load.js
+++ navbar_load.js
@@ -2,21 +2,6 @@ $(document).ready(
     function() {
         $("#navbar").load("navbar.html");
     }
-    function() {
-        switch(jQuery(this).attr('title')) {
-            case "Home":
-                jQuery('#home').addClass('active');
-                break;
-            case "Projects":
-                jQuery('#projects').addClass('active');
-                break;
-            case "Contact":
-                jQuery('#contacts').addClass('active'); 
-                break;
-            case "About":
-                jQuery('#about').addClass('active');
-                break;
-        }
-    }
+
 );
 
